6aceebe751b756c24f928e67ccb967c9ab53cd98
Chen Wei <wei.chen@cobinhood.com>
2018-07-27 13:27:12 +0800

    be able to choose

--- app/UI/Select/index.tsx
+++ app/UI/Select/index.tsx
@@ -2,63 +2,95 @@ import React, { PureComponent, ReactChildren } from 'react';
 import styled, { withTheme } from 'styled-components';
 
 const Container = styled.div`
+  position: relative;
   border-radius: 5px;
   margin: 5px;
   border-radius: 5px;
   border: 1px solid #DDD;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
   min-width: 100px;
+  padding: 0 15px;
 `;
 
-const OptionsArea = styled.div``;
+const OptionsArea = styled.div`
+  position: absolute;
+  box-sizing: border-box;
+  top: ${(p) => p.theme.HEIGHT_UNIT}px;
+  left: 0;
+  border-radius: 5px;
+  border: 1px solid #DDD;
+  width: 100%;
+  margin: 5px 0px;
+  text-align: center;
+`;
+
+const Options = styled.div`
+  padding: 5px;
+  cursor: pointer;
+  &:hover {
+    background-color: #DDD;
+  }
+`;
 
 const SelectArea = styled.div`
   height: ${(p) => p.theme.HEIGHT_UNIT}px;
   display: flex;
-  border: 1px solid red;
+  align-items: center;
   width: 100%;
+  text-align: center;
 `;
 
 const Title = styled.div`
     margin-right: 10px;
 `;
 
+interface Option {
+  value : string | number | null;
+  content : string;
+}
+
 interface Props extends React.HTMLProps<HTMLSelectElement> {
   title? : string;
   theme : any;
-  options: Array<{
-    value : string | number;
-    content : string;
-  }>
+  options: Array<Option>
   onDataChanged? : (data : string | number) => void;
 }
 
 interface State {
   openOptions : boolean;
+  selected : null | Option;
 }
 
 class Select extends PureComponent<Props, State> {
 
   public state = {
     openOptions: false,
+    selected: {
+      value: null,
+      content: '',
+    },
   };
 
   public render() {
     const { options, title } = this.props;
-    const { openOptions } = this.state;
+    const { openOptions, selected } = this.state;
     return (
       <Container>
         <SelectArea onClick={this.toggleOptions}>
           { title && (<Title>{title}</Title>) }
-          <span>Selected</span>
+          <span>{ selected && selected.content }</span>
         </SelectArea>
         { openOptions && (
           <OptionsArea>
-            {options.map((it, key) => {
-              return (<div key={it.value}>{it.content}</div>);
-            })}
+            {options.map((it : Option) =>  (
+              <Options
+                key={it.value}
+                onClick={() => {
+                  this.updateData(it);
+                }}
+              >
+                {it.content}
+              </Options>
+            ))}
           </OptionsArea>
         )}
       </Container>
@@ -69,12 +101,15 @@ class Select extends PureComponent<Props, State> {
     this.setState({ openOptions: !this.state.openOptions });
   };
 
-  private updateData = (e : React.FormEvent<EventTarget>) => {
-    const data = (e.target as HTMLSelectElement).value;
-    const { onDataChanged } = this.props;
-    if (onDataChanged) {
-      onDataChanged(data);
-    }
+  private updateData = (selected : Option) => {
+    this.setState({ selected }, () => {
+      const { value } = this.state.selected;
+      const { onDataChanged } = this.props;
+      if (onDataChanged) {
+        onDataChanged(value);
+      }
+    });
+
   }
 
 }
